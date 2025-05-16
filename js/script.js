caf5f84abd473312e6e80b9c170de4530e711734
Mauricio C <mauriciocapdevila871@gmail.com>
2025-05-16 20:55:38 -0300

    primeras modificaciones en javascrip

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+let count = 3
+while(cont>= 0){
+    count = count -1;
+}
+
+lrt edad = 40
+if (edad >= 18){
+    console.log ("Mayor de edad")
+}else {
+    console.log ("Menos de edad")
+}
+
+
 let nombre= "Mauricio"
 let edad= 25
 let barrio= "Santa isabel"
@@ -6,4 +19,16 @@ let objeto= {
     modelo: "yaris";
     año : 2010;
     color:  "rojo"
+}
+
+function suma (a,b) {
+    return a + b;
+}
+
+const suma = function (a,b) {
+    return a + b
+}
+
+const suma = (a,b) => {
+    return a + b
 }
