3b03d7adfb6c57e2c20377a181ebf8273de6759a
Mauricio C <mauriciocapdevila871@gmail.com>
2025-05-16 21:29:26 -0300

    muestra en pantalla javascript

--- js/script.js
+++ js/script.js
@@ -1,34 +1,62 @@
-let count = 3
-while(cont>= 0){
-    count = count -1;
+let contador = 3
+while(contador> 0){
+    contador = contador -1;
 }
+console.log (contador)
 
-lrt edad = 40
+let edad = 40
 if (edad >= 18){
     console.log ("Mayor de edad")
 }else {
     console.log ("Menos de edad")
 }
-
+console.log(edad)
 
 let nombre= "Mauricio"
-let edad= 25
+let años= 25
 let barrio= "Santa isabel"
 
+console.log (nombre)
+console.log (años)
+console.log (barrio)
+
+
 let objeto= {
-    modelo: "yaris";
-    año : 2010;
+    modelo: "yaris",
+    fabricacion: 2010,
     color:  "rojo"
 }
+
+console.log (objeto.modelo)
+console.log (objeto.fabricacion)
+console.log (objeto.color)
+
 
 function suma (a,b) {
     return a + b;
 }
 
-const suma = function (a,b) {
-    return a + b
+const resta = function (a,b) {
+    return a - b
 }
 
-const suma = (a,b) => {
-    return a + b
+const multiplicacion = (a,b) => {
+    return a * b
+}
+
+
+let x= 10
+let y= 5
+let opcion = 3
+
+switch (opcion){
+    case 1:
+        console.log(suma(x,y));
+        break;
+    case 2:
+        console.log(resta(x,y));
+        break;
+    case 3:
+        console.log (multiplicacion(x,y));
+        break;
 }
